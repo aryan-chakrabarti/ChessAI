5249fb614903724a3a7cbec4ee06483f8c150f77
Aryan Chakrabarti <aryanc@berkeley.edu>
2025-01-20 20:54:26 -0500

    Add nn models for each submodel

--- neural_net_train.py
+++ neural_net_train.py
@@ -1,11 +1,6 @@
 from torch.nn import Conv2d, Linear, MultiheadAttention
 from torch.nn.functional import softmax, sigmoid
 
-
-def read_data():
-    pass
-
-
 """
 2 submodels in NN model:
 1. Start square predictor model
@@ -19,17 +14,36 @@ def read_data():
 Board ----------------|              |
                |      | Start square |--- Start square
 Legal moves ----------|     model    |   |
-               |  |    --------------    |
-               |  |                      |        ____________
+               |  |    --------------    |        ____________
                |  |                       -------|            |
                |   ------------------------------| End square | -- End square
                 ---------------------------------|    model   |
                                                   ------------
 
 Start square model:
-
+                            - Conv2D - Linear -
+    Board ------- Linear - |                  +| -- Linear -----
+                            ---- Attention ----                 |
+                                                               *| - Linear - Softmax - Start square
+                            ----------------------              |
+    Legal Moves - Linear - |                     +| -- Linear --
+                            ------ Attention -----
 
 
 End square model:
-
+                            - Conv2D - Linear -
+    Board ------- Linear - |                  +| -- Linear -----
+                            ---- Attention ----                  |
+                                                                 |
+                            ----------------------               |
+    Legal Moves - Linear - |                     +| -- Linear -- | * - Linear - Softmax - End square
+                            ------ Attention -----               |
+                                                                 |
+                             ----------------------              |
+    Start Square - Linear - |                     +| -- Linear --
+                             ------ Attention -----
 """
+
+
+def read_data():
+    pass
